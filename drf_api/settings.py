257ec8410c9ad08602735b2b086e0553203105c7
oisintohak <oisintohak@gmail.com>
2022-07-15 15:27:15 +0000

    add static files settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -198,3 +198,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
