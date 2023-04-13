ffec085164dcfcc751ce97cbbc316772fa0220a0
oisintohak <oisintohak@gmail.com>
2023-04-13 10:44:12 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
  set -o errexit
  pip install -r requirements.txt
- python manage.py makemigrations && python manage.py migrate
+ python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --upload-unhashed-files --no-input
